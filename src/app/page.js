aac9b3a0129a33245f73a49fda08af060e448ec1
manoj.goofypaws <manoj.hexports@gmail.com>
2024-10-21 16:40:28 +0530

    about change

--- src/app/page.js
+++ src/app/page.js
@@ -11,11 +11,13 @@ import Social from "@/components/social";
 import Contact from "@/components/contact";
 import Footer from "@/components/footer";
 import IconsContainer from "@/components/iconsContainer";
+import WAB from "@/components/wabutton";
 
 const Page = () => {
   return (
     <>
       <ContentWrapper>
+        <WAB/>
         <NavBar />
         <Hero />
         <HeroContent />
