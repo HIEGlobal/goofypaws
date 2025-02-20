7a16b2d973fc968499d4259413f616a321caa983
manoj.goofypaws <manoj.hexports@gmail.com>
2025-02-20 13:19:10 +0530

    catalog link test

--- src/app.js
+++ src/app.js
@@ -12,4 +12,8 @@ const MyApp = ({ Component, pageProps }) => (
   </>
 );
 
+
+
 export default MyApp;
+
+
--- src/components/hero/index.js
+++ src/components/hero/index.js
@@ -20,9 +20,10 @@ const Hero = () => {
 
         <div className="flex flex-col-reverse xl:block">
           <div>
+            {/* Mobile & Tablet View */}
             <div className="sectionSubRow relative flex xl:hidden justify-between gap-4 mt-8 xl:mt-0">
               <div className="flex flex-col xl:gap-6 gap-2 xl:w-[26%] w-full">
-                <div className="">
+                <div>
                   <h2 className="text-[18px] sm:text-[25px] xl:leading-[30px] text-[#8A8A8E] font-medium text-center lg:text-left">
                     Goofy Paws Pet Care
                     <span className="text-[#000]"> Quality Products</span> for
@@ -39,63 +40,46 @@ const Hero = () => {
                     <CiCircleChevRight className="text-[#703EDB] xl:text-[50px] text-[30px]" />
                   </div>
                   <div className="xl:w-[20%] w-[40%]">
-                    <div className="flex items-center gap-2 justify-end ">
-                      <IconsContainer
-                        bgColor="#EAF2FF"
-                        image={bed}
-                        padding="py-3 px-2"
-                      />
-                      <IconsContainer
-                        bgColor="#FFEBD6"
-                        image={dog1}
-                        padding="p-3"
-                      />
-                      <IconsContainer
-                        bgColor="#EEEEEE"
-                        image={home}
-                        padding="p-3"
-                      />
+                    <div className="flex items-center gap-2 justify-end">
+                      <IconsContainer bgColor="#EAF2FF" image={bed} padding="py-3 px-2" />
+                      <IconsContainer bgColor="#FFEBD6" image={dog1} padding="p-3" />
+                      <IconsContainer bgColor="#EEEEEE" image={home} padding="p-3" />
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-            <div className="sectionSubRow relative xl:flex hidden justify-between gap-4 mt-4 xl:mt-0 ">
+
+            {/* Desktop View */}
+            <div className="sectionSubRow relative xl:flex hidden justify-between gap-4 mt-4 xl:mt-0">
               <div className="flex flex-col xl:gap-6 gap-2 xl:w-[25%] w-[45%]">
-                <div className="">
-                  <h2 className="xl:text-[18px] text-[12px] xl:leading-[30px] text-[#8A8A8E] font-medium ">
+                <div>
+                  <h2 className="xl:text-[18px] text-[12px] xl:leading-[30px] text-[#8A8A8E] font-medium">
                     Goofy Paws Pet Care <br />
                     <span className="text-[#000]"> Quality Products</span> for
                     Your Cherished Furry Family Members
                   </h2>
                 </div>
-                {/* <div className="flex items-center gap-6">
-                  <Button text="Explore Catalog" padding="px-4 py-2" textSize="6px" />
+                {/* Un-commented this part to show the button */}
+                <div className="flex items-center gap-6">
+                  <a href="https://www.flipsnack.com/E76AECFF8D6/gp-1.html" target="_blank" rel="noopener noreferrer">
+                     <Button text="Explore Catalog" padding="px-4 py-2" textSize="6px" />
+                  </a>
                   <CiCircleChevRight className="text-[#703EDB] xl:text-[50px] text-[30px]" />
-                </div> */}
+                </div>
+
               </div>
 
               <div className="xl:w-[20%] w-[40%]">
-                <div className="flex items-center gap-2 justify-end ">
-                  <IconsContainer
-                    bgColor="#EAF2FF"
-                    image={bed}
-                    padding="py-3 px-2"
-                  />
-                  <IconsContainer
-                    bgColor="#FFEBD6"
-                    image={dog1}
-                    padding="p-3"
-                  />
-                  <IconsContainer
-                    bgColor="#EEEEEE"
-                    image={home}
-                    padding="p-3"
-                  />
+                <div className="flex items-center gap-2 justify-end">
+                  <IconsContainer bgColor="#EAF2FF" image={bed} padding="py-3 px-2" />
+                  <IconsContainer bgColor="#FFEBD6" image={dog1} padding="p-3" />
+                  <IconsContainer bgColor="#EEEEEE" image={home} padding="p-3" />
                 </div>
               </div>
             </div>
           </div>
+
           <div className="flex justify-between items-end xl:mt-[-260px] mt-[-14px]">
             <div className="flex justify-end">
               <Image src={dogNeckBelt} alt="dogNeckBelt" />
